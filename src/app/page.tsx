672523001f13b0e1bbd8ce3866ce8dd278fd6ee5
gustavo de assis <gustavo.siqueira.421@gmail.com>
2024-04-21 11:54:00 -0300

    feat: page layout, task list component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+import TaskList from "@/components/TaskList";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <header className="flex justify-center mt-8 mb-14">
+        <h1 className="font-normal text-5xl">To Do List</h1>
+      </header>
+      <section className="flex flex-col items-center gap-5">
+        <TaskList></TaskList>
+        <aside className="flex flex-col items-center w-36 h-36 gap-3">
+          <h2 className="font-normal text-xl">Nova Tarefa</h2>
+          <div className="flex justify-center items-center w-24 h-28 bg-amber-300">
+            sticker
+          </div>
+        </aside>
+      </section>
+    </main>
+  );
 }
---
+++ src/components/TaskList.tsx
@@ -0,0 +1,3 @@
+export default function TaskList() {
+  return <main className="w-80 h-96 bg-[#f8f8f8]"></main>;
+}
