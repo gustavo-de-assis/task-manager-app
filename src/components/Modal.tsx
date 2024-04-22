b88596084a85716d7bd02260458c53d69f2cff8b
gustavo de assis <gustavo.siqueira.421@gmail.com>
2024-04-22 10:09:03 -0300

    feat: editing new task attributes on form and submition

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,12 +1,9 @@
+import NewTask from "./NewTask";
+
 export default function Modal() {
   return (
     <main className="flex justify-center items-center w-screen min-h-screen bg-black bg-opacity-30">
-      <section
-        className="flex justify-center items-center w-80 h-80 bg-cover bg-center text-5xl font-light"
-        style={{ backgroundImage: `url('../../assets/images/post_it.png')` }}
-      >
-        Minha tarefa
-      </section>
+      <NewTask />
     </main>
   );
 }
---
+++ src/components/NewTask.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { useState } from "react";
+
+export default function NewTask() {
+  const [taskInfo, setTaskInfo] = useState({
+    title: "",
+    description: "",
+    deadline: "",
+  });
+
+  function formHandler(e: React.ChangeEvent<HTMLFormElement>): void {
+    e.preventDefault();
+    console.log("Informações da tarefa:", taskInfo);
+  }
+
+  return (
+    <main
+      className="w-80 h-80 bg-cover bg-center font-light relative"
+      style={{ backgroundImage: `url('../../assets/images/post_it.png')` }}
+    >
+      <form className="absolute top-5 left-9 w-64" onSubmit={formHandler}>
+        <input
+          className="h-16 w-full mb-6 text-center bg-transparent font-medium text-2xl"
+          type="text"
+          maxLength={20}
+          placeholder="#Título"
+          value={taskInfo.title}
+          onChange={(e) => {
+            setTaskInfo({
+              ...taskInfo,
+              title: e.target.value,
+            });
+          }}
+          required
+        />
+        <textarea
+          className="flex flex-wrap h-32 w-full mb-3 text-start bg-transparent font-normal text-md px-1 resize-none"
+          rows={3}
+          cols={50}
+          maxLength={200}
+          placeholder="#Descrição da tarefa"
+          value={taskInfo.description}
+          onChange={(e) => {
+            setTaskInfo({
+              ...taskInfo,
+              description: e.target.value,
+            });
+          }}
+          required
+        />
+        <div className="flex justify-between">
+          <input
+            className="bg-transparent"
+            type="date"
+            alt="prazo"
+            value={taskInfo.deadline}
+            onChange={(e) => {
+              setTaskInfo({
+                ...taskInfo,
+                deadline: e.target.value,
+              });
+            }}
+          />
+          <button className="bg-blue-500 h-7 w-7 rounded mr-3" type="submit">
+            {" "}
+            OK{" "}
+          </button>
+        </div>
+      </form>
+    </main>
+  );
+}
