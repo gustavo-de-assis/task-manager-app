c088ba8e16d555854e96af4c0d454f2e9215c23f
gustavo de assis <gustavo.siqueira.421@gmail.com>
2024-04-23 20:13:37 -0300

    feat: ver. 1.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,8 +19,41 @@ export default function Home() {
         <ModalContainer />
       </section>
 
-      {/*CONTENT*/}
-      <div className={modal.visibility === "visible" ? `filter blur-sm` : ""}>
+      {/*CONTENT DESKTOP*/}
+
+      <div
+        className={
+          modal.visibility === "visible"
+            ? `filter blur-sm hidden lg:inline`
+            : "hidden lg:inline"
+        }
+      >
+        <section className="flex flex-row items-center justify-evenly gap-5">
+          {/*BOTÃO NOVA TAREFA */}
+          <aside className="flex flex-col items-center w-64 h-36 gap-3">
+            <h2 className="font-normal text-2xl">Nova Tarefa</h2>
+            <NewTaskButton />
+          </aside>
+
+          {/*CONTAINER DE TAREFAS*/}
+          <section className="flex flex-col items-center w-full h-full ">
+            <header className="flex justify-center pt-8 mb-14">
+              <h1 className="font-normal text-5xl sm:text-6xl">To Do List</h1>
+            </header>
+            <TaskListContainer />
+          </section>
+        </section>
+      </div>
+
+      {/*CONTENT MOBILE*/}
+
+      <div
+        className={
+          modal.visibility === "visible"
+            ? `filter blur-sm lg:hidden`
+            : "lg:hidden"
+        }
+      >
         <header className="flex justify-center pt-8 mb-14">
           <h1 className="font-normal text-5xl sm:text-6xl">To Do List</h1>
         </header>
