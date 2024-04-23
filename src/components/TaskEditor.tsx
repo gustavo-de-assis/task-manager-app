4c583ab9f216fac61a433db448b42e7624ab26dc
gustavo de assis <gustavo.siqueira.421@gmail.com>
2024-04-23 18:35:26 -0300

    feat: update alert

--- src/components/TaskEditor.tsx
+++ src/components/TaskEditor.tsx
@@ -66,7 +66,7 @@ export default function TaskEditor() {
         text: "Algo deu errado!",
         footer: "",
       });
-      console.log(error.response);
+      console.log(error.response.message);
     }
   }
 
